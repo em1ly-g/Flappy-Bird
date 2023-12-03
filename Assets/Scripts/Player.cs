7a63a1367f7b01f31cedf9ba322102c75fd8c15e
emilysaboss <emily.gillott2@gmail.com>
2023-12-03 14:33:38 +0000

    Adding flappy and sprites

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Public variables will appear in the editor
+
+    // Sprite fields
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex;
+
+    // Motion/position fields
+    private Vector3 direction;
+    public float gravity = -9.8f; //allows you to customise the gravity
+    public float strength = 5f;
+
+    // Called once automatically when initalising
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Automatically called the very first frame that the object is enabled
+    private void Start()
+    {
+        // Will call function every 0.15 s
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
+
+    private void AnimateSprite()
+    {
+        spriteIndex++;
+        
+        if (spriteIndex >= sprites.Length)
+        {
+            spriteIndex = 0;
+        }
+
+        spriteRenderer.sprite = sprites[spriteIndex];
+    }
+
+
+    // Unity automatically calls this every frame
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            direction = Vector3.up * strength;
+        }
+
+        // can also check for touch if playing on mobile
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                direction = Vector3.up * strength;
+            }
+        }
+
+        // We need to apply gravity every frame
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime; // This makes it frame rate independent
+
+    }
+}
